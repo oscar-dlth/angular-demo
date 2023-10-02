83afe02bd925c5dc280a5761561414a5ae66f761
oscar-dlth <delatorreoscar27@outlook.com>
2023-10-02 16:30:47 -0600

    update: rename app-state folder to state

--- src/app/UI/components/side-navigation-bar/facade/side-navigation-facade.service.ts
+++ src/app/UI/components/side-navigation-bar/facade/side-navigation-facade.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { ResponsiveService } from 'src/app/UI/modules/core/services/responsive.service';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { collapse, toggle } from 'src/app/UI/app-state/actions/side-nav-actions';
+import { collapse, toggle } from 'src/app/UI/state/actions/side-nav-actions';
 
 @Injectable({
   providedIn: 'root'
